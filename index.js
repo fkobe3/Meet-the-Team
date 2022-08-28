41e7c86d614e71e819a39dc1750bc22ff6474e4d
Frank Kobe <fkobe3@gmail.com>
2022-08-28 18:44:28 -0400

    final coding rewrite

--- index.js
+++ index.js
@@ -1,10 +1,220 @@
 const inquirer = require('inquirer');
-const Employee = require('./lib/Employee');
 const Engineer = require('./lib/Engineer');
-const Manager = require('./lib/Manager');
 const Intern = require('./lib/Intern');
-const generatePage = require('./src/page-template');
-const {writeFile, copyFile} = require('./src/generate-site');
+const Manager = require('./lib/Manager');
+const {generateHtmlPage, writeFile} = require('./src/page-template');
 
+let employees = [];
 
-//prompts to generate
+const getManagerInfo = () => {
+    console.log(` Please enter all information for Intern addition. All questions are required to complete.`);
+
+    inquirer
+        .prompt([
+        {
+            type: 'text',
+            name: 'name',
+            message: 'What is the Managers name?',
+            validate: nameInput => {
+                if (nameInput) {
+                    return true;
+                } else {
+                    console.log("Please enter the Managers name!");
+                    return false;
+                    }
+                },
+        },
+        {
+            type: 'input',
+            name: 'id',
+            message: 'What is the Managers ID (xxx)?',
+            validate: idInput => {
+                if ((!isNaN(idInput)) && (idInput.toString().length === 3)) {
+                    return true;
+                } else {
+                    return ('Please enter a 3 digit ID:');
+                }
+                },
+        },
+        {
+            type: 'text',
+            name: 'email',
+            message: 'What is the Managers email?',
+            validate: emailInput => {
+                if (emailInput.includes('@')) {
+                    return true;
+                } else {
+                    return('Please enter a valid email!');
+                }
+            }
+        },
+        {
+            type: 'input',
+            name: 'office',
+            message: 'What is the Managers office number?',
+            validate: officeInput => {
+                if (!isNaN(officeInput)) {
+                    return true;
+                } else {
+                    return('Please enter an office number:');
+                }
+            },
+        },
+    ]).then(
+        ({ name, id, email, office }) => {
+            const manager = new Manager(name, id, email, office);
+            employees.push(manager);
+            createEmployee();
+        });
+};
+
+const getEngineerInfo = () => {
+    console.log(` Please enter all information for Engineer addition. All questions are required to complete.`);
+
+    inquirer
+        .prompt([        
+            {
+                type: 'text',
+                name: 'name',
+                message: 'What is the Engineers name?',
+                validate: nameInput => {
+                    if (nameInput) {
+                        return true;
+                    } else {
+                        return ("Please enter the Engineers name!");
+                        }
+                    },
+            },
+            {
+                type: 'input',
+                name: 'id',
+                message: 'What is the Engineers ID (xxx)?',
+                validate: idInput => {
+                    if ((!isNaN(idInput)) && (idInput.toString().length === 3)) {
+                        return true;
+                    } else {
+                        return ('Please enter a 3 digit ID:');
+                    }
+                    },
+            },
+            {
+                type: 'text',
+                name: 'email',
+                message: 'What is the Engineers email?',
+                validate: emailInput => {
+                    if (emailInput.includes('@')) {
+                        return true;
+                    } else {
+                        return('Please enter a valid email!');
+                    }
+                }
+            },
+
+            {           
+                type: 'input',
+                name: 'github',
+                message: 'What is the Engineers github username?',
+                validate: githubInput => {
+                if (githubInput) {
+                    return true;
+                } else {
+                    console.log('',' Please enter a github user name!');
+                    return false;
+                }
+            },
+            },
+        ]).then(
+            ({ name, id, email, github }) => {
+                const engineer = new Engineer(name, id, email,github);
+                employees.push(engineer);
+                createEmployee();
+            });
+};
+
+const getInternInfo = () => {
+    console.log(` Please enter all information for Intern addition. All questions are required to complete.`);
+
+    inquirer
+        .prompt([        
+            {
+                type: 'text',
+                name: 'name',
+                message: 'What is the Interns name?',
+                validate: nameInput => {
+                    if (nameInput) {
+                        return true;
+                    } else {
+                        return ("Please enter the Interns name!");
+                        }
+                    },
+            },
+            {
+                type: 'input',
+                name: 'id',
+                message: 'What is the Interns ID (xxx)?',
+                validate: idInput => {
+                    if ((!isNaN(idInput)) && (idInput.toString().length === 3)) {
+                        return true;
+                    } else {
+                        return ('Please enter a 3 digit ID:');
+                    }
+                    },
+            },
+            {
+                type: 'text',
+                name: 'email',
+                message: 'What is the Interns email?',
+                validate: emailInput => {
+                    if (emailInput.includes('@')) {
+                        return true;
+                    } else {
+                        return('Please enter a valid email!');
+                    }
+                }
+            },
+
+            {           
+                type: 'input',
+                name: 'school',
+                message: 'What is the Interns School?',
+                validate: schoolInput => {
+                if (schoolInput) {
+                    return true;
+                } else {
+                    console.log('',' Please enter a school!');
+                    return false;
+                }
+            },
+            },
+        ]).then(
+            ({ name, id, email, school }) => {
+                const intern = new Intern(name, id, email, school);
+                employees.push(intern);
+                createEmployee();
+            });
+};
+
+const createEmployee = () => {
+    inquirer
+        .prompt([
+        {
+        type: 'list',
+        name: 'type',
+        message: 'Choose an employee type to add.',
+        choices: ['Engineer', 'Intern', 'No More']
+        }
+    ]).then(employeeRole => {
+    
+    if (employeeRole.type === 'Engineer') {
+        getEngineerInfo()
+    } else if (employeeRole.type === 'Intern') {
+        getInternInfo()
+    } else {
+        
+        const htmlPage = generateHtmlPage(employees);
+        writeFile(htmlPage);
+    }
+    })
+};
+
+getManagerInfo();
--- src/page-template.js
+++ src/page-template.js
@@ -1,97 +1,118 @@
-// create the About section
-const generateAbout = aboutText => {
-    if (!aboutText) {
-      return '';
-    }
-  
-    return `
-      <section class="my-3" id="about">
-        <h2 class="text-dark bg-primary p-2 display-inline-block">About Me</h2>
-        <p>${aboutText}</p>
-      </section>
-    `;
-  };
-  
-  // create the Employee section
-  const generateProjects = projectsArr => {
-    return `
-      <section class="my-3" id="portfolio">
-        <h2 class="text-dark bg-primary p-2 display-inline-block">Work</h2>
-        <div class="flex-row justify-space-between">
-        ${projectsArr
-          .filter(({ feature }) => feature)
-          .map(({ name, description, languages, link }) => {
-            return `
-            <div class="col-12 mb-2 bg-dark text-light p-3">
-              <h3 class="portfolio-item-title text-light">${name}</h3>
-              <h5 class="portfolio-languages">
-                Built With:
-                ${languages.map(language => language).join(',')}
-              </h5>
-              <p>${description}</p>
-              <a href="${link}" class="btn"><i class="fab fa-github mr-2"></i>View Project on GitHub</a>
-            </div>
-          `;
-          })
-          .join('')}
-        ${projectsArr
-          .filter(({ feature }) => !feature)
-          .map(({ name, description, languages, link }) => {
-            console.log(languages);
-            return `
-            <div class="col-12 col-md-6 mb-2 bg-dark text-light p-3 flex-column">
-              <h3 class="portfolio-item-title text-light">${name}</h3>
-              <h5 class="portfolio-languages">
-                Built With:
-                ${languages.join(', ')}
-              </h5>
-              <p>${description}</p>
-              <a href="${link}" class="btn mt-auto"><i class="fab fa-github mr-2"></i>View Project on GitHub</a>
-            </div>
-          `;
-          })
-          .join('')}
-      
-        </div>
-      </section>
-    `;
-  };
+const fs = require("fs");
 
-//export function to create HTML page
-module.exports = templateData => {
-// Sets page up by section
-const {} = templateData;
-return `
+const generateHtmlPage = (employees) => {
+
+    return `
 <!DOCTYPE html>
 <html lang="en">
-
 <head>
-  <meta charset="UTF-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <meta http-equiv="X-UA-Compatible" content="ie=edge">
-  <title>Portfolio Demo</title>
-  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.11.2/css/all.min.css">
-  <link href="https://fonts.googleapis.com/css?family=Public+Sans:300i,300,500&display=swap" rel="stylesheet">
-  <link rel="stylesheet" href="style.css">
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css" integrity="sha384-TX8t27EcRE3e/ihU7zmQxVncDAy5uIKz4rEkgIXeMed4M0jlfIDPvg6uqKI2xXr2" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.7.0/css/all.css" integrity="sha384-lZN37f5QGtY3VHgisS14W3ExzMWZxybE1SJSEsQp9S+oqd12jhcu+A56Ebc1zFSJ" crossorigin="anonymous">
+    <link rel='stylesheet' href='../src/style.css'>
+    <title>Project Team</title>
 </head>
-
 <body>
-  <header>
-    <div class="container flex-row justify-space-between align-center py-3">
-      <h1 class="page-title text-secondary bg-dark py-2 px-3">${header.name}</h1>
-      <nav class="flex-row">
-        <a class="ml-2 my-1 px-2 py-1 bg-secondary text-dark" href="https://github.com/${header.github}">GitHub</a>
-      </nav>
-    </div>
-  </header>
-  <main class="container my-5">
-    ${generateAbout(about)}
-    ${generateProjects(projects)}
-  </main>
-  <footer class="container text-center py-3">
-    <h3 class="text-dark">&copy;2020 by ${header.name}</h3>
-  </footer>
+    <header><h1>My Team</h1></header>
+    <div class="container">
+        <div class="row">
+        ${generateCard(employees)}
+    </div>  <!-- end row -->
+    </div>  <!-- end container -->
+      <!-- jQuery and JS bundle w/ Popper.js -->
+<script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ho+j7jyWK8fNQe+A12Hb8AhRq26LrZ/JpcUGGOn+Y7RsweNrtN/tE3MoK7ZeZDyx" crossorigin="anonymous"></script>
 </body>
 </html>
-`;
+    `
+};
+
+const generateCard = (employees) => {
+
+    return `   
+        ${employees
+            .filter((employee) => employee.getRole() === "Manager")
+            .map(({name, id, email, officeNumber}) => {
+            return `
+            <div class="card">
+                <div class="card-header">
+                    <h5 class="card-title">${name}</h5>
+                    <h6 class="card-subtitle mb-2 text-muted"><i class="fas fa-mug-hot"></i> Manager</h6>
+                </div>
+            <div class="card-body">
+            <ul class="list-group list-group-flush">
+                <li class="list-group-item">Empl Id: ${id}</li>
+                <li class="list-group-item">Email: <a href="mailto:${email}">${email}</a></li>
+                <li class="list-group-item">Office number: ${officeNumber}</li>
+            </ul>   
+            </div>
+            </div>
+            `;
+            })
+            .join('')}
+        
+        ${employees
+            .filter((employee) => employee.getRole() === "Engineer")
+            .map(({name, id, email, github}) => {
+            return `
+            <div class="card">
+                <div class="card-header">
+                    <h5 class="card-title">${name}</h5>
+                    <h6 class="card-subtitle mb-2 text-muted"><i class="fas fa-glasses"></i> Engineer</h6>
+                </div>
+            <div class="card-body">
+            <ul class="list-group list-group-flush">
+                <li class="list-group-item">Empl Id: ${id}</li>
+                <li class="list-group-item">Email: <a href="mailto:${email}">${email}</a></li>
+                <li class="list-group-item">GitHub: <a href="https://github.com/${github}">${github}</a></li>
+            </ul>   
+            </div>
+            </div>
+            `;
+            })
+            .join('')}
+        
+        ${employees
+            .filter((employee) => employee.getRole() === "Intern")
+            .map(({name, id, email, school}) => {
+            return `
+            <div class="card">
+                <div class="card-header">
+                    <h5 class="card-title">${name}</h5>
+                    <h6 class="card-subtitle mb-2 text-muted"><i class="fas fa-user-graduate"></i> Intern</h6>
+                </div>
+            <div class="card-body">
+            <ul class="list-group list-group-flush">
+                <li class="list-group-item">Empl Id: ${id}</li>
+                <li class="list-group-item">Email: <a href="mailto:${email}">${email}</a></li>
+                <li class="list-group-item">School: ${school}</li>
+            </ul>   
+            </div>
+            </div>
+            `;
+            })
+            .join('')}
+    `
+};
+
+const writeFile = fileContent => {
+    
+    return new Promise ((resolve, reject) => {
+        fs.writeFile('./dist/index.html', fileContent, err => {
+            if (err) {
+                reject(err);
+                return;
+            }
+            resolve({
+                ok: true,
+                message: 'File Created!'
+            });
+        });
+    })
+};
+
+module.exports = {
+    generateHtmlPage,
+    writeFile
 };
